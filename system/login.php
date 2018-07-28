22f88f4d6d44ab2c3b37ae858225b4321b646f8c
MarisolCanete <canete.marisol98@gmail.com>
2018-07-28 14:22:09 +0800

    Removed Comments

--- system/login.php
+++ system/login.php
@@ -58,54 +58,8 @@ session_start();
               header ('location: pages/grade/grade.php');
             else:
               echo "<h1>Invalid Username or Password</h1>";
-            endif;
-            // var_dump($row);
-            // print($u);
-            // print($p);
-            // $username = $_POST['txt_username'];
-            // $password = $_POST['txt_password'];
-
-
-            // $admin = mysqli_query($con, "SELECT * from tbladmin where username = '$username' and password = '$password' and accounttype = 'Administrator' ");
-            // $numrow = mysqli_num_rows($admin);
-
-            // $teacher = mysqli_query($con, "SELECT * from tblteacher where username = '$username' and password = '$password' ");
-            // $numrow1 = mysqli_num_rows($teacher);
-
-            // $student = mysqli_query($con, "SELECT * from tblstudent where username = '$username' and password = '$password' ");
-            //     $numrow2 = mysqli_num_rows($student);
-
-            // if($numrow > 0)
-            // {
-            //     while($row = mysqli_fetch_array($admin)){
-            //       $_SESSION['role'] = "Administrator";
-            //       $_SESSION['userid'] = $row['id'];
-            //     }    
-            //     header ('location: pages/dashboard/dashboard.php');
-            // }
-            // elseif($numrow1 > 0)
-            //   {
-            //     while($row = mysqli_fetch_array($teacher)){
-            //       $_SESSION['role'] = "Teacher";
-            //       $_SESSION['userid'] = $row['id'];
-            //     } 
-            //     header ('location: pages/student/student.php');
-            //   }
-            // elseif($numrow2 > 0)
-            //     {
-            //       while($row = mysqli_fetch_array($student)){
-            //         $_SESSION['role'] = "Student";
-            //         $_SESSION['userid'] = $row['id'];
-            //       } 
-            //       header ('location: pages/grade/grade.php');
-            //     }
-            //  else
-            //     {
-            //       echo 'invalid account';
-            //     }
-             
+            endif;             
         }
-        
       ?>
 
     </body>
