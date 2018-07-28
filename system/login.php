a3be11e23c63ea7d6e33af2d89196dcf91b98cf2
MarisolCanete <canete.marisol98@gmail.com>
2018-07-28 14:38:58 +0800

    Changed to alert-danger class for bootstrap.

--- system/login.php
+++ system/login.php
@@ -31,36 +31,36 @@ session_start();
                 <button type="submit" class="btn btn-sm btn-primary" name="btn_login">Log in</button>
               </form>
             </div>
-          </div>
+        </div>
+<?php
+include "pages/connection.php";
+if(isset($_POST['btn_login']))
+{ 
+  $u = mysqli_real_escape_string($con,$_POST['txt_username']);
+  $p = mysqli_real_escape_string($con,$_POST['txt_password']);
+
+  $query = mysqli_query($con,"SELECT * FROM tbl_user_account WHERE username='$u'");
+  $row = mysqli_fetch_assoc($query);
+  if ($row['u_code']==1):
+    $_SESSION['role'] = "Administrator";
+    $_SESSION['userid'] = $row['user_id'];
+    header ('location: pages/dashboard/dashboard.php');
+  elseif ($row['u_code']==3):
+    $_SESSION['role'] = "Teacher";
+    $_SESSION['userid'] = $row['user_id'];
+    header ('location: pages/student/student.php');
+  elseif ($row['u_code']==2):
+    $_SESSION['role'] = "Student";
+    $_SESSION['userid'] = $row['user_id'];
+    header ('location: pages/grade/grade.php');
+  else:
+    echo "<div class='alert alert-danger' role='alert'>
+            Invalid Username or Password!
+          </div>";
+  endif;             
+}
+?>
           </div>
         </div>
-
-      <?php
-        include "pages/connection.php";
-        if(isset($_POST['btn_login']))
-        { 
-            $u = mysqli_real_escape_string($con,$_POST['txt_username']);
-            $p = mysqli_real_escape_string($con,$_POST['txt_password']);
-
-            $query = mysqli_query($con,"SELECT * FROM tbl_user_account WHERE username='$u'");
-            $row = mysqli_fetch_assoc($query);
-            if ($row['u_code']==1):
-              $_SESSION['role'] = "Administrator";
-              $_SESSION['userid'] = $row['user_id'];
-              header ('location: pages/dashboard/dashboard.php');
-            elseif ($row['u_code']==3):
-              $_SESSION['role'] = "Teacher";
-              $_SESSION['userid'] = $row['user_id'];
-              header ('location: pages/student/student.php');
-            elseif ($row['u_code']==2):
-              $_SESSION['role'] = "Student";
-              $_SESSION['userid'] = $row['user_id'];
-              header ('location: pages/grade/grade.php');
-            else:
-              echo "<center><h1>Invalid Username or Password</h1></center>";
-            endif;             
-        }
-      ?>
-
     </body>
 </html>
